7c62fd0640f3f9cf2ea702053c8e6787c41a59a6
github username <github email address>
2019-01-12 22:22:13 -0800

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,6 +2,17 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
+# ANSWER
 def roll
-  # code goes here
+  return rand 1..6
 end
+
+# BONUS - another way:
+# def roll
+#   return rand 1...7
+# end
+
+# BONUS - another way:
+# def roll(*p)
+#   return rand 1..6
+# end
